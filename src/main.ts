2a1f894ea628538ae3047ec6d2d152b248fa168b
camarm <armand@camponovo.xyz>
2023-08-02 13:21:18 +0200

    [Fix] Using Ios ui

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ import '@ionic/vue/css/display.css';
 import './theme/variables.css';
 
 const app = createApp(App)
-  .use(IonicVue)
+  .use(IonicVue,  { mode: 'ios' })
   .use(router);
   
 router.isReady().then(() => {
