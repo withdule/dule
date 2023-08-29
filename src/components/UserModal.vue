c7603f3c34c92cf0e9dafb6ea629f8b4b0535e65
camarm <armand@camponovo.xyz>
2023-08-29 15:09:36 +0200

    [Add] Account statistics

--- src/components/UserModal.vue
+++ src/components/UserModal.vue
@@ -44,21 +44,22 @@
         <CheckSquare slot="start" class="icon-icon ion-color-tertiary"/>
         <ion-label>
           <p>Tasks completed</p>
-          <h2>50</h2>
+          <h2>{{ stats.tasks }} / {{ stats.tasksCompleted }}</h2>
+          <ion-progress-bar :value="stats.tasks / stats.tasksCompleted"></ion-progress-bar>
         </ion-label>
       </ion-item>
       <ion-item>
         <BookMarked slot="start" class="icon-icon ion-color-tertiary"/>
         <ion-label>
           <p>Notes taken</p>
-          <h2>32</h2>
+          <h2>{{ stats.notes }}</h2>
         </ion-label>
       </ion-item>
       <ion-item>
         <CalendarCheck slot="start" class="icon-icon ion-color-tertiary"/>
         <ion-label>
           <p>Event scheduled</p>
-          <h2>8</h2>
+          <h2>{{ stats.events }}</h2>
         </ion-label>
       </ion-item>
     </ion-list>
@@ -66,11 +67,9 @@
 </template>
 
 <script setup lang="ts">
-import { IonTitle } from "@ionic/vue";
-import {XCircle, Cog, BookMarked, CalendarCheck, CheckSquare, LogOut} from "lucide-vue-next";
+import {IonTitle} from "@ionic/vue";
+import {BookMarked, CalendarCheck, CheckSquare, Cog, LogOut, XCircle} from "lucide-vue-next";
 import DuleFaceIcon from "@/components/DuleFaceIcon.vue";
-import {createModal} from "@/functions/modals";
-import SettingsModal from "@/components/SettingsModal.vue";
 import DuleFaceIconDark from "@/components/DuleFaceIconDark.vue";
 </script>
 
@@ -78,6 +77,8 @@ import DuleFaceIconDark from "@/components/DuleFaceIconDark.vue";
 import {closeModals} from "@/functions/modals";
 import {getAccount, logOut} from "@/functions/fetch/account";
 import {ref} from "vue";
+import {get} from "@/functions/fetch/tools";
+import {APIResponse} from "@/functions/fetch/interfaces";
 
 let refs = {
   modalSettings: ref(null)
@@ -97,6 +98,12 @@ export default {
         email: "",
         createdAt: "",
       },
+      stats: {
+        notes: 0,
+        events: 0,
+        tasks: 0,
+        tasksCompleted: 0
+      } as any & APIResponse,
       refs: refs,
       darkTheme: false
     }
@@ -107,6 +114,7 @@ export default {
   created () {
     getAccount().then(user => this.user = user)
     this.darkTheme = this.isDarkTheme()
+    this.fetchStats()
   },
   methods: {
     closeModals,
@@ -116,6 +124,11 @@ export default {
     },
     isDarkTheme() {
       return localStorage.getItem('userAppearance') === 'dark'
+    },
+    async fetchStats() {
+      const url = import.meta.env.VITE_API_URL + '/me/stats'
+      const response = await get(url)
+      this.stats = response.data
     }
   }
 }
